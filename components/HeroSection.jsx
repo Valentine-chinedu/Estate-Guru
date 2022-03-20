c7e43fb4453f5c080abaed4d8a97354d40584e49
okosival <valentine11.dev@gmail.com>
2022-03-20 17:40:48 +0000

    hero section completed

--- components/HeroSection.jsx
+++ components/HeroSection.jsx
@@ -2,18 +2,19 @@ import React from 'react'
 
 const HeroSection = () => {
   return (
-    <div className="flex h-screen w-screen flex-col items-center justify-center space-y-8 bg-[url('/realestate4.jpg')]">
-      <h1 className="w-6/12 text-6xl font-semibold uppercase text-gray-50">
-        looking for a new home?
-      </h1>
+    <div className="flex h-screen w-full flex-col items-center justify-center space-y-8 bg-[url('/realestate4.jpg')] ">
+      <div className="flex w-6/12 flex-col items-center text-6xl font-bold uppercase text-blue-200">
+        <h1>looking for a new</h1>
+        <h1>Property?</h1>
+      </div>
       <div className="h-0.5 w-7/12 bg-gray-50 "></div>
       <div className="space-x-8 text-gray-100">
-        <button className="bg-red-500 px-8 py-1 font-medium uppercase">
+        <a className="cursor-pointer bg-red-500 px-8 py-1 font-medium uppercase hover:bg-red-400">
           buy
-        </button>
-        <button className="bg-red-500 px-8 py-1 font-medium uppercase">
+        </a>
+        <a className="cursor-pointer bg-red-500 px-8 py-1 font-medium uppercase hover:bg-red-400">
           rent
-        </button>
+        </a>
       </div>
     </div>
   )
