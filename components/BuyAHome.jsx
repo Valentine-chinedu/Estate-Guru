b44af2fd40c4ca7f62181ccdc85e7fd57dbc6cc1
okosival <valentine11.dev@gmail.com>
2022-03-20 17:41:15 +0000

    feat: created grid layout

--- components/BuyAHome.jsx
+++ components/BuyAHome.jsx
@@ -1,7 +1,17 @@
 import React from 'react'
+import ListingDetails from './ListingDetails'
 
-const BuyAHome = () => {
-  return <div>BuyAHome</div>
+const BuyAHome = ({ propertiesForSale }) => {
+  console.log(propertiesForSale)
+  return (
+    <div className=" mx-4 grid h-full grid-cols-2 justify-items-center gap-x-4 gap-y-6 py-8 md:grid-cols-3 md:gap-x-4 lg:mx-48 lg:grid-cols-3 lg:gap-y-8">
+      {propertiesForSale?.map((property) => (
+        <div className="" key={property.id}>
+          <ListingDetails property={property} />
+        </div>
+      ))}
+    </div>
+  )
 }
 
 export default BuyAHome
