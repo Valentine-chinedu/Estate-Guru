340d459a10ce3b2a496fdc99652c95b2f6e9ad66
okosival <valentine11.dev@gmail.com>
2022-03-21 23:14:52 +0000

    completed

--- components/RentAHome.jsx
+++ components/RentAHome.jsx
@@ -1,7 +1,29 @@
 import React from 'react'
+import Listings from './Listings'
 
-const RentAHome = () => {
-  return <div>RentAHome</div>
+const RentAHome = ({ propertiesForRent }) => {
+  return (
+    <div className="w-full bg-gray-300">
+      <div className=" mx-4 grid h-full grid-cols-2 justify-items-center gap-x-4 gap-y-6 py-8 md:grid-cols-3 md:gap-x-4 lg:mx-48 lg:grid-cols-3 lg:gap-y-8">
+        {propertiesForRent?.map((property) => (
+          <div className="" key={property.id}>
+            <Listings
+              title={property.title}
+              baths={property.baths}
+              area={property.area}
+              isVerified={property.isVerified}
+              externalID={property.externalID}
+              coverPhoto={property.coverPhoto}
+              price={property.price}
+              rentFrequency={property.rentFrequency}
+              rooms={property.rooms}
+              purpose="for rent"
+            />
+          </div>
+        ))}
+      </div>
+    </div>
+  )
 }
 
 export default RentAHome
