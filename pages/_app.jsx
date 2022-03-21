94b8e60425900b086ac4abcaea31798a5df17c27
okosival <valentine11.dev@gmail.com>
2022-03-21 23:15:39 +0000

    added header and footer

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,8 +1,17 @@
+import Footer from '../components/Footer'
+import Header from '../components/Header'
 import '../styles/globals.css'
 // import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      {' '}
+      <Header />
+      <Component {...pageProps} />
+      <Footer />{' '}
+    </>
+  )
 }
 
 export default MyApp
