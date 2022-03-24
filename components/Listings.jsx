e1474fb3ce95ed0259e87cb35234492ac165bfd4
okosival <valentine11.dev@gmail.com>
2022-03-24 23:12:38 +0000

    final commit

--- components/Listings.jsx
+++ components/Listings.jsx
@@ -1,6 +1,5 @@
 import Image from 'next/image'
 import Link from 'next/link'
-import React from 'react'
 
 import { FaBed, FaBath } from 'react-icons/fa'
 import { BsGridFill } from 'react-icons/bs'
