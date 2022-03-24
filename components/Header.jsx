d20d3da14408dc000ef4afcf28734819a183247f
okosival <valentine11.dev@gmail.com>
2022-03-24 09:07:39 +0000

    added links

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,27 +1,47 @@
+import Link from 'next/link'
 import React from 'react'
 
 const Header = () => {
   return (
-    <div className=" fixed z-50 flex h-16 w-full items-center justify-between bg-gray-100 px-8">
-      <h1 className="font-serif text-2xl font-bold italic text-red-500">
-        EstateGuru
-      </h1>
+    <div className=" fixed z-50 flex h-12 w-full items-center justify-between bg-gray-100 px-8">
+      <Link href="/">
+        <a className="font-serif text-2xl font-bold italic text-blue-600">
+          EstateGuru
+        </a>
+      </Link>
       <nav>
         <ul className="flex space-x-12 text-sm font-medium text-gray-700">
-          <li className="cursor-pointer py-1 px-2 hover:bg-red-500 hover:text-gray-100">
-            Home
-          </li>
-          <li className="cursor-pointer py-1 px-2 hover:bg-red-500 hover:text-gray-100">
-            Buy
-          </li>
-          <li className="cursor-pointer py-1 px-2 hover:bg-red-500 hover:text-gray-100">
-            Rent
-          </li>
-          <li className="cursor-pointer py-1 px-2 hover:bg-red-500 hover:text-gray-100">
+          <Link href="/">
+            <a className="cursor-pointer py-1 px-2 hover:bg-blue-600 hover:text-gray-100">
+              Home
+            </a>
+          </Link>
+          <Link
+            href={{
+              pathname: '/property/searchAllProperty',
+              query: { type: 'for-sale' },
+            }}
+          >
+            <a className="cursor-pointer py-1 px-2 hover:bg-blue-600 hover:text-gray-100">
+              Buy
+            </a>
+          </Link>
+          <Link
+            href={{
+              pathname: '/property/searchAllProperty',
+              query: { type: 'for-rent' },
+            }}
+          >
+            <a className="cursor-pointer py-1 px-2 hover:bg-blue-600 hover:text-gray-100">
+              Rent
+            </a>
+          </Link>
+
+          <li className="hover:bg-600-600 cursor-pointer py-1 px-2 hover:bg-blue-600 hover:text-gray-100">
             About
           </li>
-          <li className="cursor-pointer py-1 px-2 hover:bg-red-500 hover:text-gray-100">
-            Search
+          <li className="cursor-pointer py-1 px-2 hover:bg-blue-600 hover:text-gray-100">
+            contact
           </li>
         </ul>
       </nav>
