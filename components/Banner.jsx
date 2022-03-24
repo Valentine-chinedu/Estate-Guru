3e3fa6a90ab8bd965ca4bb21296d838ccca2b40d
okosival <valentine11.dev@gmail.com>
2022-03-24 23:21:59 +0000

    fix: fixed import error

--- components/Banner.jsx
+++ components/Banner.jsx
@@ -1,5 +1,5 @@
-import Link from 'next/link'
 import React from 'react'
+import Link from 'next/link'
 import { FaLongArrowAltRight } from 'react-icons/fa'
 
 const Banner = ({ title, listing, type }) => {
